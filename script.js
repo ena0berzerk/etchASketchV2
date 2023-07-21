aac3afd2e50b269318113d30891106d4631e077c
enduraew <enduraew@gmail.com>
2023-07-21 14:54:02 +0500

    Add feat for game

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
 const container = document.querySelector('.container');
-let currentSizeOfGrid = 16;
+let defaultSizeOfGrid = 16;
 const sizeBtn = document.querySelector('.size');
+const blackBtn = document.querySelector('.black');
+const rainbowBtn = document.querySelector('.rainbow');
+const eraserBtn = document.querySelector('.eraser');
 
 function promptSize() {
   return +prompt('Choose size between 4 and 100', 16);
+}
+
+function getSquares() {
+  return document.querySelectorAll('.square');
 }
 
 // grid
@@ -19,25 +26,46 @@ function grid(size) {
     }
   }
 }
-grid(currentSizeOfGrid);
+grid(defaultSizeOfGrid);
 
-// Click button and change size
-sizeBtn.addEventListener('click', () => {
-  newSize = promptSize();
-  if (newSize < 4 || newSize > 100 || isNaN(newSize)) {
-    alert('Error. Choose between 4 and 100');
-    return false;
-  } else if (newSize === '' || newSize === false || newSize === null) {
-    alert('hello');
-    return false;
-  } else {
-    container.replaceChildren();
-    sizedGrid(newSize);
-    paint();
-  }
-});
+function changeSize() {
+  sizeBtn.addEventListener('click', () => {
+    newSize = promptSize();
+    if (newSize == null || newSize == '' || newSize == false || !newSize) {
+      alert('Canceled!');
+      false;
+    } else if (newSize < 4 || newSize > 100 || isNaN(newSize)) {
+      alert('Choose between 4 and 100!');
+      false;
+    } else {
+      container.replaceChildren();
+      sizedGrid(newSize);
+      chooseBrush();
+      cleanGrid();
+    }
+  });
+}
+changeSize();
 
-// Make new grid // !!! think later how to DRY. I created 2 same functions with small changes (line 11)
+function erase() {
+  const eraserBtn = document.querySelector('.eraser');
+  eraserBtn.addEventListener('click', () => {
+    const erase = getSquares();
+    if (eraserBtn.textContent === 'Eraser') {
+      eraserBtn.textContent = 'Eraser Off';
+    } else if (eraserBtn.textContent === 'Eraser Off') {
+      eraserBtn.textContent = 'Eraser';
+    }
+    erase.forEach((eraseEl) => {
+      eraseEl.addEventListener('mouseenter', () => {
+        eraseEl.classList.toggle('paint');
+      });
+    });
+  });
+}
+erase();
+
+// Making new grid // !!! think later how to DRY. I created 2 same functions with small changes (line 12)
 function sizedGrid(size) {
   for (let w = 0; w < size; w++) {
     let row = document.createElement('div');
@@ -52,13 +80,23 @@ function sizedGrid(size) {
   }
 }
 
-// paint
-function paint() {
-  const paintSquare = document.querySelectorAll('.square');
-  paintSquare.forEach((square) => {
+function chooseBrush() {
+  const allSquare = getSquares();
+  allSquare.forEach((square) => {
     square.addEventListener('mouseenter', () => {
       square.classList.add('paint');
     });
   });
 }
-paint();
+chooseBrush();
+
+function cleanGrid() {
+  const allSquare = getSquares();
+  const cleanBtn = document.querySelector('.clean');
+  const cleanFunc = cleanBtn.addEventListener('click', () => {
+    allSquare.forEach((squareEl) => {
+      squareEl.classList.remove('paint');
+    });
+  });
+}
+cleanGrid();
